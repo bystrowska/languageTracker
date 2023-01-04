69c8dec28d366af919af4cc99d67a9fd4124c6fa
paulinabystrowska <50452580+bystrowska@users.noreply.github.com>
2023-01-04 11:59:36 +0800

    tutorial: path parameters

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/projects/{project_id}")
+async def get_project(project_id: int):
+    return {"project_id": project_id}
