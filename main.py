7bdaca8d2cf82d78da0619b2472d5eb4c8917289
paulinabystrowska <50452580+bystrowska@users.noreply.github.com>
2023-01-04 12:54:30 +0800

    tutorial: query parameters

--- main.py
+++ main.py
@@ -9,13 +9,40 @@ class ModelName(str, Enum):
 
 app = FastAPI()
 
+items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/projects/{project_id}")
-async def get_project(project_id: int):
-    return {"project_id": project_id}
+async def get_project(project_id: int, q: str | None = None, short: bool = False):
+    project = {"item_id": project_id}
+    if q:
+        project.update({"q": q})
+    if not short:
+        project.update(
+            {"desc": "This is a description idk what to write"}
+        )
+    return project
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"desc": "This is a user's item blah blah"}
+        )
+    return item
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, needy: str):
+    item = {"item_id": item_id, "needy": needy}
+    return item
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
@@ -29,3 +56,7 @@ async def get_model(model_name: ModelName):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file_path": file_path}
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return items_db[skip: skip + limit]
