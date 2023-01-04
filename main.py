240ec6ccc15efdeca7c70fc7b25603335b231ab1
paulinabystrowska <50452580+bystrowska@users.noreply.github.com>
2023-01-04 12:04:06 +0800

    tutorial: path parameters - enums

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from enum import Enum
+
 from fastapi import FastAPI
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 app = FastAPI()
 
@@ -9,3 +16,12 @@ async def root():
 @app.get("/projects/{project_id}")
 async def get_project(project_id: int):
     return {"project_id": project_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
