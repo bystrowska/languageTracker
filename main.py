2aa5143ca384b7f67680c2b6d62f109f2874afc0
paulinabystrowska <50452580+bystrowska@users.noreply.github.com>
2023-01-05 12:14:32 +0800

    tutorial: request body

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from enum import Enum
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+'''
+data model from Pydantic gives code completion and other stuff
+other than that basically a dict (?)
+'''
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 app = FastAPI()
 
@@ -60,3 +71,23 @@ async def read_file(file_path: str):
 @app.get("/items/")
 async def read_item(skip: int = 0, limit: int = 10):
     return items_db[skip: skip + limit]
+
+# request with body
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+# request with body and path and query parameters
+# if in path detected as path arg
+# if not in path and singular type (int, float...) -> query arg
+# if there's a model for that type -> request body
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: str | None = None):
+    result =  {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
